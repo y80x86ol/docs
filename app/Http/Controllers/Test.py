5dec90bf36e8ce0a11b8862aad68f1d59aa5cd8d
Ken <y80x86ol@gmail.com>
2017-01-18 13:53:48 +0800

    代码初始化

--- app/Http/Controllers/Test.py
+++ app/Http/Controllers/Test.py
@@ -1,8 +1,4 @@
 from flask import render_template
 
 def index():
-    # return "test info"
     return render_template("index.html")
-
-def template():
-    return render_template('test/template.html')
